46ec4507f11c2b81af10412c528804e637303e7b
Hubert Łępicki <hubert.lepicki@amberbit.com>
2014-08-10 10:07:48 +0200

    more homepage form

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -4,9 +4,10 @@ describe 'Logging in', type: :feature do
   context 'with email and password' do
     it 'should prompt user for log in' do
       visit '/'
-      expect(page).to have_content("Please sign in")
+      expect(page).to have_content("Please log in")
       expect(page).to have_field("E-mail")
       expect(page).to have_field("Password")
+      expect(page).to have_link("Log in")
     end
   end
 end
